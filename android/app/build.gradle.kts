2de9b50b59c280759e7be229ab75aafa40166e69
Gautam99-coder <gautamtharu1000@gmail.com>
2025-10-17 23:12:01 +0530

    setup the firebase ,ndk veersion,compileSdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,16 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    //google json
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.urmedio"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+//    compileSdk = flutter.compileSdkVersion
+    compileSdk=35
+//    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,7 +28,8 @@ android {
         applicationId = "com.example.urmedio"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+//        minSdk = flutter.minSdkVersion
+        minSdk=23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
