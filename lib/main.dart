b4d684ac825525981eb0308966082da07f6fd954
Gautam99-coder <gautamtharu1000@gmail.com>
2025-10-17 23:13:24 +0530

    setup the firebase successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:urmedio/app_routes.dart'; // Import the new route generator
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase/firebase.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
-  await firebase.InitializeApp();
+  await Firebase.initializeApp();
   runApp(const UrMedioApp());
 }
 
