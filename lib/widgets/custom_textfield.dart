efd17a6c09ad809f06aa687cf730272c6b3611ca
Gautam99-coder <gautamtharu1000@gmail.com>
2025-10-18 23:07:25 +0530

    TextFields  with reusable

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -1,37 +1,63 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final String label;
+  final IconData prefixIcon;
+  final bool isPassword;
   final TextEditingController controller;
-  final bool obscureText;
+  final String? Function(String?)? validator;
   final TextInputType keyboardType;
-  final IconData? icon;
 
   const CustomTextField({
     super.key,
     required this.label,
+    required this.prefixIcon,
     required this.controller,
-    this.obscureText = false,
+    this.isPassword = false,
+    this.validator,
     this.keyboardType = TextInputType.text,
-    this.icon,
   });
 
   @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = true;
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextField(
-        controller: controller,
-        obscureText: obscureText,
-        keyboardType: keyboardType,
-        decoration: InputDecoration(
-          labelText: label,
-          prefixIcon: icon != null ? Icon(icon) : null,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return TextFormField(
+      controller: widget.controller,
+      obscureText: widget.isPassword ? _obscureText : false,
+      validator: widget.validator,
+      keyboardType: widget.keyboardType,
+      decoration: InputDecoration(
+        labelText: widget.label,
+        prefixIcon: Icon(widget.prefixIcon),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+          icon: Icon(
+            _obscureText ? Icons.visibility_off : Icons.visibility,
           ),
+          onPressed: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+        )
+            : null,
+        contentPadding: EdgeInsets.symmetric(
+          vertical: screenWidth * 0.035,
+          horizontal: screenWidth * 0.04,
         ),
       ),
     );
   }
 }
+
